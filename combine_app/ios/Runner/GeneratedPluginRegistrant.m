6b4ce21fad1b6ca931fd0da3da80ff3869423d02
t-lypha <53065236+t-lypha@users.noreply.github.com>
2019-09-06 15:09:41 -0700

    Add files via upload

---
+++ combine_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <location/LocationPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+}
+
+@end
